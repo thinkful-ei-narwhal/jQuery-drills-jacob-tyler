5f7cd737c00d3a42a6fded5b51b1f1e15c725306
Tyler Sharp <johntylersharp@gmail.com>
2020-03-18 13:04:05 -0400

    added carousel

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable strict */
 $('.thumbnail').on('click', e => {
-    console.log(e.target);
-  });
+  const src = $(event.currentTarget).find('img').attr('src');
+  console.log(src);
+  const alt = $(event.currentTarget).find('img').attr('alt');
+  console.log(alt)
+  $('.hero img').attr('src', src);
+  $('.hero img').attr('alt',alt);
+});
+
+//Active
+//$('.hero').carousel();
+
+// //Enable Indicator
+// $('a').click(function(){
+//   $('.hero img src').remove();
+//   $(event.currentTarget).clone().appendTo('.hero')
+// });
   
